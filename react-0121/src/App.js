8108154a8e3641252ae4ec2c936b98138de7f198
jchans <jiangchenghans@gmail.com>
2021-01-21 15:08:21 +0800

    Class component practice

--- react-0121/src/App.js
+++ react-0121/src/App.js
@@ -1,9 +1,14 @@
 import CountFunc from './components/CountFunc'
+import CountClass from './components/CountClass'
 
 function App() {
   return (
     <>
+      <h2>函式型元件</h2>
       <CountFunc />
+
+      <h2>類別型元件</h2>
+      <CountClass />
     </>
   )
 }
